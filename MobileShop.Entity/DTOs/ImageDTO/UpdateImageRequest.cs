253bcdcfa6a60edffd3ea71096873813bf4cf7ec
Nhiaa <nghiavthe160378@fpt.edu.vn>
2024-01-25 00:26:47 +0700

    feat(service): create service product, image, category

---
+++ MobileShop.Entity/DTOs/ImageDTO/UpdateImageRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileShop.Entity.DTOs.ImageDTO
+{
+    public class UpdateImageRequest
+    {
+        public int ImageId { get; set; }
+
+        public string? ImageLink { get; set; }
+
+        public DateTime? CreateDate { get; set; }
+
+        public bool? IsDeleted { get; set; }
+    }
+}
---
+++ MobileShop.Entity/DTOs/ProductDTO/UpdateProductRequest.cs
@@ -0,0 +1,34 @@
+﻿using MobileShop.Entity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileShop.Entity.DTOs.ProductDTO
+{
+    public class UpdateProductRequest
+    {
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; } = null!;
+
+        public double Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string? Description { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public int ImageId { get; set; }
+
+        public DateTime? CreateDate { get; set; }
+
+        public int CreateBy { get; set; }
+
+        public bool? IsDeleted { get; set; }
+
+        
+    }
+}
